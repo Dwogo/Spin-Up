1b4db0b6e43a4a53e29455332b900c646721841a
Dwog <57990308+Dwogo@users.noreply.github.com>
2022-06-02 08:47:36 +1200

    Done Reversed & Done Motors & assignments

--- include/assignment.hpp
+++ include/assignment.hpp
@@ -1,2 +1,13 @@
 #include "main.h"
 
+// Global variables
+int rev = 1;
+
+// Controller
+Controller oleana(CONTROLLER_MASTER);
+
+// Motors
+Motor GFM(1, MOTOR_GEARSET_18, false);
+Motor GBM(2, MOTOR_GEARSET_18, false);
+Motor RBM(3, MOTOR_GEARSET_18, true);
+Motor RFM(6, MOTOR_GEARSET_18, true);
