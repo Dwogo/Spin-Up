6902ea6ac2a9b34c2d78074952eee0e1ec2271c5
Dwog <57990308+Dwogo@users.noreply.github.com>
2022-06-16 12:51:39 +1200

    Added the idea of switching roller colour + incase we get stuck

--- include/assignment.hpp
+++ include/assignment.hpp
@@ -2,9 +2,13 @@
 
 // Global variables
 int rev = 1;
+bool blue = true;
 
 // Controller
-Controller oleana(CONTROLLER_MASTER);
+Controller OLEANA(CONTROLLER_MASTER);
+
+// Optical Sensor
+Vision LOKI(18);
 
 // Motors
 // Drivetrain
@@ -13,6 +17,10 @@ Motor GBM(2, MOTOR_GEARSET_18, false);
 Motor RBM(3, MOTOR_GEARSET_18, true);
 Motor RFM(6, MOTOR_GEARSET_18, true);
 
-// The Shootingtrain
-Motor scoop(9, MOTOR_GEARSET_18, false);
-Motor FW(10, MOTOR_GEARSET_36, false);
+// Shootingtrain
+Motor CON(10, MOTOR_GEARSET_18, true);
+Motor FW(9, MOTOR_GEARSET_36, false);
+
+// Widetrain
+Motor GWM(18, MOTOR_GEARSET_18, false);
+Motor RWM(19, MOTOR_GEARSET_18, false);
--- src/main.cpp
+++ src/main.cpp
@@ -1,88 +1,65 @@
 #include "assignment.hpp"
 
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
+void rollerSwitch() {
+  bool switched = false;
+  while (!switched) 
+  {
+    // LOKi
+  }
+}
+
 void initialize() {
   lcd::initialize();
   lcd::set_text(1, "Team: 2976G ");
   lcd::set_text(2, "Name: Goonz");
   lcd::set_text(3, "School: Christchurch Boys' High School");
   lcd::set_text(4, "Made by: Phillip, Janzen, Blake, Frank, and Ethan");
+  lcd::set_text(5, "Emotional Support: JC");
 }
 
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
-void disabled() {}
+void disabled() { lcd::set_text(6, "The robot is now crippled"); }
 
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
 void competition_initialize() {}
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
 void autonomous() {}
 
 void reversed() {
   rev = (rev == 1) ? -1 : 1;
 
   if (rev == -1) {
-    oleana.rumble("..");
+    OLEANA.rumble("..");
     delay(50);
-    oleana.print(0, 0, "Reversed");
+    OLEANA.print(0, 0, "Reversed");
   }
 
   if (rev == 1) {
-    oleana.rumble("-");
+    OLEANA.rumble("-");
     delay(50);
-    oleana.print(0, 0, "Normal");
+    OLEANA.print(0, 0, "Normal");
   }
 }
 
 void opcontrol() {
   while (true) {
-    if (oleana.get_digital_new_press(DIGITAL_DOWN))
-      reversed();
+    if (OLEANA.get_digital_new_press(DIGITAL_DOWN)) reversed();
+    if (OLEANA.get_digital_new_press(DIGITAL_UP)) rollerSwitch();
 
-    GFM.move(((oleana.get_analog(ANALOG_LEFT_Y) - oleana.get_analog(ANALOG_LEFT_X)) * rev) - oleana.get_analog(ANALOG_RIGHT_X));
-    GBM.move(((oleana.get_analog(ANALOG_LEFT_Y) + oleana.get_analog(ANALOG_LEFT_X)) * rev) - oleana.get_analog(ANALOG_RIGHT_X));
-    RFM.move(((oleana.get_analog(ANALOG_LEFT_Y) + oleana.get_analog(ANALOG_LEFT_X)) * rev) + oleana.get_analog(ANALOG_RIGHT_X));
-    RBM.move(((oleana.get_analog(ANALOG_LEFT_Y) - oleana.get_analog(ANALOG_LEFT_X)) * rev) + oleana.get_analog(ANALOG_RIGHT_X));
+    GFM.move(((OLEANA.get_analog(ANALOG_LEFT_Y) - OLEANA.get_analog(ANALOG_LEFT_X)) * rev) - OLEANA.get_analog(ANALOG_RIGHT_X));
+    GBM.move(((OLEANA.get_analog(ANALOG_LEFT_Y) + OLEANA.get_analog(ANALOG_LEFT_X)) * rev) - OLEANA.get_analog(ANALOG_RIGHT_X));
+    RFM.move(((OLEANA.get_analog(ANALOG_LEFT_Y) + OLEANA.get_analog(ANALOG_LEFT_X)) * rev) + OLEANA.get_analog(ANALOG_RIGHT_X));
+    RBM.move(((OLEANA.get_analog(ANALOG_LEFT_Y) - OLEANA.get_analog(ANALOG_LEFT_X)) * rev) + OLEANA.get_analog(ANALOG_RIGHT_X));
 
-
-    if (oleana.get_digital(DIGITAL_R2)) {
+    if (OLEANA.get_digital(DIGITAL_R2)) {
       FW.move(127);
-      scoop.move(127);
-    }
-    else if (oleana.get_digital(DIGITAL_R1)) {
+      CON.move(127);
+    } else if (OLEANA.get_digital(DIGITAL_R1)) {
       FW.move(0);
-      scoop.move(127);
+      CON.move(127);
+    } else if (OLEANA.get_digital(DIGITAL_LEFT)) {
+      CON.move(-127);
     } else {
       FW.move(0);
-      scoop.move(0);
+      CON.move(0);
     }
-
   }
 }
