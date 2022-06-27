d8d1401cd8eae2afc6c9f19eaa8d34656b7676a6
Dwog <57990308+Dwogo@users.noreply.github.com>
2022-06-28 10:00:30 +1200

    Trying to add the color switching for the rollers

---
+++ include/assignment.h
@@ -0,0 +1,29 @@
+#include "main.h"
+
+// Global variables
+int rev = 1;          // Used for reversing the movement of the bot
+bool blue = true;     // What alliance colour we are in
+bool usrCtrl = false; // See if the we have driver control
+
+// Controller
+Controller OLEANA(CONTROLLER_MASTER);
+
+// Vision Sensor
+Vision LOKI(18);
+
+// Motors
+// Drivetrain
+// Red
+Motor GFM(1, MOTOR_GEARSET_18, false); // The front motor of the green gear side
+Motor GBM(2, MOTOR_GEARSET_18, false); // The back motor of the green gear side
+// Green
+Motor RBM(3, MOTOR_GEARSET_18, true); // The front motor of the red gear side
+Motor RFM(6, MOTOR_GEARSET_18, true); // The back motor of the red gear side
+
+// Shootingtrain
+Motor CON(10, MOTOR_GEARSET_18, true); // Motor for the conveyor
+Motor FW(9, MOTOR_GEARSET_36, false);  // Motor for the flywheel
+
+// Widetrain
+Motor GWM(18, MOTOR_GEARSET_18, false); // Green side's motor for the horizontal expansian
+Motor RWM(19, MOTOR_GEARSET_18, false); // Red side's motor for the horizontal expansian
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,71 @@
-#include "assignment.hpp"
+#include "assignment.h"
+#include "pros/llemu.hpp"
+#include "pros/misc.h"
+#include "pros/vision.h"
 
-void rollerSwitch() {
-  bool switched = false;
-  while (!switched) 
-  {
-    // LOKi
+void colorSwitch() {
+  bool switched = true;
+  while (!switched) {
+    vision_object_s_t col = LOKI.get_by_size(0);
+    lcd::set_text(8, col.signature);
+    switched = true;
+    delay(20);
   }
+}
+
+void conCtrl() {
+  while (true) {
+    if (OLEANA.get_digital(DIGITAL_A)) {
+      colorSwitch();
+      // Only if we are able to drive
+    } else if (usrCtrl) {
+      // Controlling the Conveyor and Flywheel Motors
+      if (OLEANA.get_digital(DIGITAL_R2)) {
+        FW.move(127);
+        CON.move(127);
+      } else if (OLEANA.get_digital(DIGITAL_R1)) {
+        FW.move(0);
+        CON.move(127);
+      } else if (OLEANA.get_digital(DIGITAL_LEFT)) {
+        CON.move(-127);
+      } else {
+        FW.move(0);
+        CON.move(0);
+      }
+    }
+    delay(20);
+  }
+}
+
+// Print out the current draw of the drivetrain motors
+void motorCurrents() {
+
+  lcd::clear();
+
+  lcd::set_text(0, "GFM current: " + GFM.get_current_draw());
+  lcd::set_text(1, "GBM current: " + GBM.get_current_draw());
+  lcd::set_text(2, "RFM current: " + RFM.get_current_draw());
+  lcd::set_text(3, "RBM current: " + RBM.get_current_draw());
 }
 
 void initialize() {
   lcd::initialize();
-  lcd::set_text(1, "Team: 2976G ");
-  lcd::set_text(2, "Name: Goonz");
-  lcd::set_text(3, "School: Christchurch Boys' High School");
-  lcd::set_text(4, "Made by: Phillip, Janzen, Blake, Frank, and Ethan");
-  lcd::set_text(5, "Emotional Support: JC");
+
+  // Printing credits on the Brain Lcd
+  lcd::set_text(0, "Team: 2976G ");
+  lcd::set_text(1, "Name: Goonz");
+  lcd::set_text(2, "School: Christchurch Boys' High School");
+  lcd::set_text(3, "Made by: Phillip, Janzen, Blake, Frank, and Ethan");
+  lcd::set_text(4, "Emotional Support: JC");
+
+  // Registering the left most button on the lcd to the motorCurrent function
+  lcd::register_btn0_cb(motorCurrents);
 }
 
+// When the robot cant do anything so we say it's crippled
 void disabled() { lcd::set_text(6, "The robot is now crippled"); }
 
-void competition_initialize() {}
-
-void autonomous() {}
-
+// Function to reverse the movement on the field
 void reversed() {
   rev = (rev == 1) ? -1 : 1;
 
@@ -39,27 +82,40 @@ void reversed() {
   }
 }
 
+// Driver control
 void opcontrol() {
+  usrCtrl = true;
+  Task conveyor(conCtrl);
+
   while (true) {
-    if (OLEANA.get_digital_new_press(DIGITAL_DOWN)) reversed();
-    if (OLEANA.get_digital_new_press(DIGITAL_UP)) rollerSwitch();
 
-    GFM.move(((OLEANA.get_analog(ANALOG_LEFT_Y) - OLEANA.get_analog(ANALOG_LEFT_X)) * rev) - OLEANA.get_analog(ANALOG_RIGHT_X));
-    GBM.move(((OLEANA.get_analog(ANALOG_LEFT_Y) + OLEANA.get_analog(ANALOG_LEFT_X)) * rev) - OLEANA.get_analog(ANALOG_RIGHT_X));
-    RFM.move(((OLEANA.get_analog(ANALOG_LEFT_Y) + OLEANA.get_analog(ANALOG_LEFT_X)) * rev) + OLEANA.get_analog(ANALOG_RIGHT_X));
-    RBM.move(((OLEANA.get_analog(ANALOG_LEFT_Y) - OLEANA.get_analog(ANALOG_LEFT_X)) * rev) + OLEANA.get_analog(ANALOG_RIGHT_X));
+    // Call the reversed function
+    if (OLEANA.get_digital_new_press(DIGITAL_DOWN))
+      reversed();
 
-    if (OLEANA.get_digital(DIGITAL_R2)) {
-      FW.move(127);
-      CON.move(127);
-    } else if (OLEANA.get_digital(DIGITAL_R1)) {
-      FW.move(0);
-      CON.move(127);
-    } else if (OLEANA.get_digital(DIGITAL_LEFT)) {
-      CON.move(-127);
-    } else {
-      FW.move(0);
-      CON.move(0);
-    }
+    // Call the function to print the current draw of the drivetrain
+    if (OLEANA.get_digital_new_press(DIGITAL_X))
+      motorCurrents();
+
+    // Move the Motors: Move on the plane with the left joystick and rotate with
+    // the right joystick
+    GFM.move(((OLEANA.get_analog(ANALOG_LEFT_Y) -
+               OLEANA.get_analog(ANALOG_LEFT_X)) * // Movement on plane
+              rev) - // Reversing the movement on plone
+             OLEANA.get_analog(ANALOG_RIGHT_X)); // Rotation
+    GBM.move(((OLEANA.get_analog(ANALOG_LEFT_Y) +
+               OLEANA.get_analog(ANALOG_LEFT_X)) * // Movement on plane
+              rev) - // Reversing the movement on plone
+             OLEANA.get_analog(ANALOG_RIGHT_X)); // Rotation
+    RFM.move(((OLEANA.get_analog(ANALOG_LEFT_Y) +
+               OLEANA.get_analog(ANALOG_LEFT_X)) * // Movement on plane
+              rev) + // Reversing the movement on plone
+             OLEANA.get_analog(ANALOG_RIGHT_X)); // Rotation
+    RBM.move(((OLEANA.get_analog(ANALOG_LEFT_Y) -
+               OLEANA.get_analog(ANALOG_LEFT_X)) * // Movement on plane
+              rev) + // Reversing the movement on plone
+             OLEANA.get_analog(ANALOG_RIGHT_X)); // Rotation
+
+    delay(20);
   }
 }
